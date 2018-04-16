98d282fb83a9fab5be6bba292cc8df0aa66ba506
波哥 <hzyangbo15@corp.netease>
2018-04-17 02:38:17 +0800

    add: version 1.2.1

---
+++ test.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+
+var app = express();
+var router = express.Router();
+
+var mockData = require('./generate/index.js');
+
+router.use('/', function (req, res, next) {
+    var mockfile = path.join(__dirname, req.path);
+    if (fs.existsSync(mockfile)) {
+        var temple = fs.readFileSync(mockfile, 'utf-8');
+        var resData = mockData.entry(JSON.parse(temple), req);
+        res.send(resData);
+    } else {
+        res.send("无效请求路径");
+    }
+});
+
+app.use(router).listen(3000);
+console.log('listen port 3000');
